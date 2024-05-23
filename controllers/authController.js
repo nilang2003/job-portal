9b5892ab47a437c5fcc92ba7101a1510dc7e9983
Nilang Dave <110675577+nilang2003@users.noreply.github.com>
2024-05-23 12:56:59 +0530

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,71 @@
+import userModel from "../models/userModel.js";
+
+export const registerController = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  //validate it
+  if (!name) {
+    next("Name is required");
+  }
+
+  if (!email) {
+    next("Email is required");
+  }
+
+  if (!password) {
+    next("Password is required");
+  }
+
+  const existingUser = await userModel.findOne({ email });
+  if (existingUser) {
+    next("Email already Register Please Login");
+  }
+
+  const user = await userModel.create({ name, email, password });
+
+  //token
+  const token = user.createJWT();
+
+  res.status(201).send({
+    success: true,
+    message: "New User Created.",
+    user: {
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      location: user.location,
+    },
+    token,
+  });
+};
+
+export const LoginController = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //validation
+  if (!email || !password) {
+    next("Please provide all fields");
+  }
+
+  //find user by email
+  const user = await userModel.findOne({ email }).select("password");
+  if (!user) {
+    next("Invalid username or password");
+  }
+
+  //compare password
+  const isMatch = await user.comparePassword(password);
+  {
+    if (!isMatch) {
+      next("Invalid username or password");
+    }
+  }
+
+  user.password = undefined;
+  const token = user.createJWT();
+  res.status(200).json({
+    success: true,
+    message: "Login Successful",
+    user,
+    token,
+  });
+};
